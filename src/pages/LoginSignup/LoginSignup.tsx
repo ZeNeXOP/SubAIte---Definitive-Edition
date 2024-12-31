fbb433c0ab92317690c2982028f9cbd07e25cf95
ZeNeXOP <146000355+ZeNeXOP@users.noreply.github.com>
2024-12-31 17:35:50 +0530

    added frontend for login and signup from youtube tutorial

--- src/pages/LoginSignup/LoginSignup.tsx
+++ src/pages/LoginSignup/LoginSignup.tsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./LoginSignup.css";
 
 export const LoginSignup = () => {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+  
+  
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google-plus-g"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google-plus-g"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>or use your email password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <button className="hidden" onClick={handleLoginClick}>
+              Sign In
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button className="hidden" onClick={handleRegisterClick}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
